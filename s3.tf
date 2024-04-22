06cff74ee3c7ac4eec2348760bf71f0c8eaae407
Gowtham <54115415+gouthamted@users.noreply.github.com>
2024-04-23 02:33:11 +0700

    s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,10 @@
 provider "aws" {
+  profile= "myaws"
   region = "me-central-1"
 }
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
+  ac1="Private"
 
   tags = {
     Name        = "My bucket"
